af5b4a6131d35c4867cdfcac548958e867a670c0
Florian Nücke <florian@nuecke.de>
2020-10-18 14:22:36 +0200

    Render tiles in parallel.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,10 @@ import li.cil.kotraycer.geometry.BVH
 import li.cil.kotraycer.geometry.ShapeList
 import li.cil.kotraycer.geometry.Sphere
 import li.cil.kotraycer.material.*
-import li.cil.kotraycer.math.*
+import li.cil.kotraycer.math.BlueNoise
+import li.cil.kotraycer.math.Vector2
+import li.cil.kotraycer.math.Vector3
+import li.cil.kotraycer.math.nextVector3
 import li.cil.kotraycer.render.MAX_RECURSION
 import li.cil.kotraycer.render.RenderContext
 import li.cil.kotraycer.render.SimpleCamera
@@ -12,6 +15,7 @@ import java.awt.Desktop
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.math.min
 import kotlin.random.Random
 import kotlin.system.measureTimeMillis
 
@@ -30,8 +34,8 @@ fun main() {
     val colors = listOf(
         Color(0.9f, 0.2f, 0.2f),
         Color(0.2f, 1f, 0.3f),
-        Color(0.1f, 0.2f, 1f),
-        Color(0.8f, 0.8f, 0.2f),
+        Color(0.3f, 0.5f, 1f),
+        Color(1f, 0.9f, 0.2f),
         Color(0.2f, 0.7f, 0.9f),
     )
 
@@ -57,7 +61,7 @@ fun main() {
     val spheres = BVH((0..200).map {
         val position = (rng.nextVector3() - 0.5f) * area.size()
         val material = materialPool[rng.nextInt(materialPool.size)](rng)
-        Sphere(position, 0.2f + rng.nextFloat() * 0.2f, material)
+        Sphere(position, 0.2f + rng.nextFloat() * 0.7f, material)
     })
 
     val scene = ShapeList(
@@ -78,19 +82,25 @@ fun main() {
     val context = RenderContext(scene, rng, MAX_RECURSION)
     val samples = BlueNoise.generate(context.rng, SAMPLE_COUNT)
 
+    val jobSize = 16
+    val jobs = (0 until resolutionX step jobSize).flatMap { x ->
+        (0 until resolutionY step jobSize).map { y -> Pair(x, y) }
+    }
     val totalTime = measureTimeMillis {
-        for (y in 0 until resolutionY) {
-            for (x in 0 until resolutionX) {
-                var color = Colors.BLACK
-                for (sample in samples) {
-                    val uv = Vector2(
-                        (x + sample.x) / (resolutionX - 1f),
-                        (y + sample.y) / (resolutionY - 1f)
-                    )
-                    val ray = camera.getRay(uv)
-                    color += trace(ray, context)
+        jobs.parallelStream().forEach { (x0, y0) ->
+            (y0 until min(y0 + jobSize, resolutionY)).forEach { y ->
+                (x0 until min(x0 + jobSize, resolutionX)).forEach { x ->
+                    var color = Colors.BLACK
+                    for (sample in samples) {
+                        val uv = Vector2(
+                            (x + sample.x) / (resolutionX - 1f),
+                            (y + sample.y) / (resolutionY - 1f)
+                        )
+                        val ray = camera.getRay(uv)
+                        color += trace(ray, context)
+                    }
+                    image.setRGB(x, resolutionY - 1 - y, Colors.toRGBInt(color / samples.size.toFloat()))
                 }
-                image.setRGB(x, resolutionY - 1 - y, Colors.toRGBInt(color / samples.size.toFloat()))
             }
         }
     }
